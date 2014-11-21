622b36875d6addc675db9166bc9bb46fd9259282
Ralph Janke <hexnosis@jankeconsulting.ca>
2014-11-21 14:05:36 -0500

    Added documentation for doxygen Fixed lint warnings

--- src/tabpanel.cpp
+++ src/tabpanel.cpp
@@ -4,9 +4,16 @@
  * Author: Ralph Janke hexnosis@jankeconsulting.ca
  */
 
-#include "tabpanel.h"
+#include "./tabpanel.h"
 #include <QDebug>
 
+/**
+ * @brief constructs th object
+ * @param parent of the object
+ * @param file associated with the panel
+ *
+ *
+ */
 TabPanel::TabPanel(QWidget *parent, QFile *file) :
     QWidget(parent)
 {
@@ -37,6 +44,12 @@ TabPanel::TabPanel(QWidget *parent, QFile *file) :
 
 }
 
+/**
+ * @brief destroys the object
+ *
+ * The associated model, hexpanel, textpael and layoout are freed
+ * before the object is destroys.
+ */
 TabPanel::~TabPanel()
 {
     delete model();
@@ -45,27 +58,51 @@ TabPanel::~TabPanel()
     delete layout();
 }
 
+/**
+ * @brief creates the buffer for the file in the model
+ * @param len - length of the file
+ * @param fillchar - character to fill the buffer with
+ *
+ * This method is called when a new file is created.
+ */
 void TabPanel::createFile(int len, char fillchar)
 {
     model()->createBuffer(len, fillchar);
 }
 
+/**
+ * @brief saves the file associated with this object
+ * @param filename of the file to be saved
+ * @return true if file was saved sucessfully, otherwise false
+ */
 bool TabPanel::saveFile(QString filename)
 {
     return model()->saveDatatoFile(filename);
 }
 
+/**
+ * @brief sets the flag for alternating row colors
+ * @param state - true if alternatig row colors are displayed, otherwise false
+ */
 void TabPanel::setAlternatingRowColors(bool state)
 {
     hexPanel()->setAlternatingRowColors(state);
     textPanel()->setAlternatingRowColors(state);
 }
 
+/**
+ * @brief sets the flag for the visibility of the hexpanel
+ * @param state - true if th hexpanel is visible, otherwise false
+ */
 void TabPanel::setHexPanelVisibility(bool state)
 {
     hexPanel()->setVisible(state);
 }
 
+/**
+* @brief sets the flag for the visibility of the textpanel
+* @param state - true if th textpanel is visible, otherwise false
+*/
 void TabPanel::setTextPanelVisibility(bool state)
 {
     textPanel()->setVisible(state);
@@ -76,26 +113,50 @@ void TabPanel::setTextPanelVisibility(bool state)
     hexPanel()->calculateMinimumWidth();
 }
 
+/**
+ * @brief sets the model for this tab
+ * @param model
+ */
 void TabPanel::setModel(HexFileModel *model) {
     m_model = model;
 }
 
+/**
+ * @brief provides the model for the tab
+ * @return the model
+ */
 HexFileModel *TabPanel::model() {
     return m_model;
 }
 
+/**
+ * @brief sets the hexpanel for the tab
+ * @param panel
+ */
 void TabPanel::setHexPanel(HexPanel *panel) {
     m_hexpanel = panel;
 }
 
+/**
+ * @brief returns the hexpanel of the tab
+ * @return hexpanel
+ */
 HexPanel *TabPanel::hexPanel() {
     return m_hexpanel;
 }
 
+/**
+ * @brief sets the textpanel for the tab
+ * @param panel
+ */
 void TabPanel::setTextPanel(TextPanel *panel) {
     m_textpanel = panel;
 }
 
+/**
+ * @brief returns the textpanel of the tab
+ * @return textpanel
+ */
 TextPanel *TabPanel::textPanel() {
     return m_textpanel;
 }
