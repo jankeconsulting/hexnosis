c1355d84c6709ba695f9812dbd43ec8a60312897
Ralph Janke <hexnosis@jankeconsulting.ca>
2014-11-22 16:04:19 -0500

    Fixed lint warnings

--- src/hexfilemodel.cpp
+++ src/hexfilemodel.cpp
@@ -81,10 +81,14 @@ int HexFileModel::columnCount(const QModelIndex &index) const
  */
 QVariant HexFileModel::data(const QModelIndex &index, int role) const
 {
-    if (!index.isValid() || (role != Qt::DisplayRole && role != Qt::EditRole))
+    if (not(index.isValid()) || (role != Qt::DisplayRole && role != Qt::EditRole))
+    {
         return QVariant();
+    }
     if (index.row()*columnCount()+index.column() >= filebuffer.size())
+    {
         return QVariant();
+    }
     return filebuffer.at(index.row()*columnCount()+index.column());
 }
 
@@ -102,17 +106,25 @@ QVariant HexFileModel::data(const QModelIndex &index, int role) const
  */
 QByteArray HexFileModel::data( int length, const QModelIndex &index, int role) const
 {
-    if (!index.isValid() || (role != Qt::DisplayRole))
+    if (not(index.isValid()) || (role != Qt::DisplayRole))
+    {
         return QByteArray();
+    }
     if (index.row()*columnCount()+index.column() >= filebuffer.size())
+    {
         return QByteArray();
+    }
     int len = filebuffer.size() - (index.row()*columnCount()+index.column());
-    if(len < length)
+    if (len < length)
+    {
         length = len;
+    }
     QByteArray data = QByteArray();
     data.resize(length);
     for (int i=0; i<length; i++)
+    {
         data[i] = filebuffer.at(index.row()*columnCount()+index.column()+i);
+    }
     return data;
 }
 
@@ -129,7 +141,8 @@ QByteArray HexFileModel::data( int length, const QModelIndex &index, int role) c
  */
 bool HexFileModel::setData(const QModelIndex &index, const QVariant &value, int role)
 {
-    if(index.isValid() && role == Qt::EditRole) {
+    if (index.isValid() && role == Qt::EditRole)
+    {
         filebuffer.replace(index.row()*columnCount()+index.column(), 1, value.toByteArray());
         emit dataChanged(index, index);
         return true;
@@ -149,16 +162,19 @@ bool HexFileModel::setData(const QModelIndex &index, const QVariant &value, int 
 bool HexFileModel::setData(const QModelIndex &index, const QByteArray &values, int role)
 {
     QModelIndex index2;
-    if ((index.column() + value.size()) < columnCount()) {
+    if ((index.column() + value.size()) < columnCount())
+    {
         index2 = QAbstractItemModel::createIndex(index.row(), index.column()+value.size()-1);
     }
-    else {
+    else
+    {
         int row = (index.row() + (value.size() / tablewidth)) + 1;
         int column = (index.column()+value.size()-1) % tablewidth;
         index2 = QAbstractItemModel::createIndex(row, column);
     }
 
-    if(index.isValid() && role == Qt::EditRole) {
+    if (index.isValid() && role == Qt::EditRole)
+    {
         filebuffer.replace(index.row()*columnCount()+index.column(), value.size(), value);
         emit dataChanged(index, index2);
         return true;
@@ -175,9 +191,10 @@ bool HexFileModel::setData(const QModelIndex &index, const QByteArray &values, i
  */
 Qt::ItemFlags HexFileModel::flags(const QModelIndex &index) const
 {
-    if (!index.isValid())
+    if (not(index.isValid()))
+    {
         return Qt::ItemIsEnabled;
-
+    }
     return QAbstractItemModel::flags(index) | Qt::ItemIsEditable;
     //    return Qt::ItemIsEnabled | Qt::ItemIsSelectable | Qt::ItemIsEditable;
 }
@@ -193,7 +210,8 @@ Qt::ItemFlags HexFileModel::flags(const QModelIndex &index) const
  * This function must be implemented to make this class instantiable.
  * Currently it will always true, and has no real functionality.
  */
-bool HexFileModel::setHeaderData(int section, Qt::Orientation orientation, const QVariant &value, int role)
+bool HexFileModel::setHeaderData(int section, Qt::Orientation orientation,
+                                 const QVariant &value, int role)
 {
     Q_UNUSED(section);
     Q_UNUSED(orientation);
@@ -212,13 +230,17 @@ bool HexFileModel::setHeaderData(int section, Qt::Orientation orientation, const
  */
 QVariant HexFileModel::headerData(int section, Qt::Orientation orientation, int role) const
 {
-    if (role == Qt::DisplayRole) {
+    if (role == Qt::DisplayRole)
+    {
 //        TODO: Should representation of header be located in Delegate?
-        if(orientation == Qt::Horizontal) {
+        if (orientation == Qt::Horizontal)
+        {
             return QString("%1").arg(QByteArray(1, section).toHex().toUpper().data()[1]);
         }
-        if(orientation == Qt::Vertical) {
-            return QString("%1h").arg(QByteArray(3, 0).setNum(section * tablewidth, 16).toUpper().data());
+        if (orientation == Qt::Vertical)
+        {
+            return QString("%1h").arg(QByteArray(3, 0).setNum(section * tablewidth, 16).
+                                      toUpper().data());
         }
     }
     return QVariant();
@@ -237,9 +259,9 @@ void HexFileModel::loadDatafromFile(int length, int offset)
 {
     Q_UNUSED(length);
     Q_UNUSED(offset);
-    if(file) {
+    if (file)
+    {
         filebuffer = file->readAll();
-
         file->close();
     }
 }
@@ -260,10 +282,12 @@ bool HexFileModel::saveDatatoFile(QString file_name, int length, int offset)
     Q_UNUSED(length);
     Q_UNUSED(offset);
 
-    if (!file_name.isEmpty()) {
+    if (!file_name.isEmpty())
+    {
         file = new QFile(file_name);
 
-        if (file->open(QIODevice::WriteOnly)) {
+        if (file->open(QIODevice::WriteOnly))
+        {
             file->write(filebuffer);
             file->close();
             return true;
@@ -291,7 +315,8 @@ void HexFileModel::createBuffer(int len, char fillchar)
  */
 void HexFileModel::setColumnHeaders()
 {
-    for(int i=0; i<16; i++) {
+    for(int i=0; i<16; i++)
+    {
 //        QStandardItem *item = new QStandardItem(QString().number(i,16).toUpper());
         //setHorizontalHeaderItem(i, item);
         //setHorizontalHeaderItem(i+16, item);
@@ -324,8 +349,12 @@ QString HexFileModel::fileInfo()
 void HexFileModel::updateCursorInfo(QModelIndex current, QModelIndex previous)
 {
     Q_UNUSED(previous);
-    if(current.isValid())
+    if (current.isValid())
+    {
         HexnosisWindow::updateCursorInfo(current.row()*columnCount()+current.column(), data(current, Qt::DisplayRole).toInt());
+    }
     else
+    {
         HexnosisWindow::clearCursorInfo();
+    }
 }
